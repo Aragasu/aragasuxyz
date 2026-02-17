fa2ad2ac8510de6340b0f6bafd5680b428f1b690
Aragasu <168364088+Aragasu@users.noreply.github.com>
2026-02-17 01:56:28 +0100

    Delete wichteln directory

--- wichteln/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBu5IloYCJEcrAg_lMfUJi5jZG5wGhFNVk",
-  authDomain: "aragasu-wichteln.firebaseapp.com",
-  projectId: "aragasu-wichteln",
-  storageBucket: "aragasu-wichteln.firebasestorage.app",
-  messagingSenderId: "183877722231",
-  appId: "1:183877722231:web:b61a348095b0dd6dec2672"
-};
